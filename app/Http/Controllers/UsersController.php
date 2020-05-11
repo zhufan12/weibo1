8aa62e381486d013bedb210216482d59cbfb0c10
zhufan12 <a1253308969@gmail.com>
2020-05-11 12:17:53 +0000

    刪除用戶功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
 
     public function __construct(){
         $this->middleware('auth',[
-            'except' => ['show','create','store']
+            'except' => ['show','create','store','index']
         ]);
 
          $this->middleware('guest', [
@@ -72,5 +72,11 @@ class UsersController extends Controller
         public function index(){
               $users = User::paginate(10);
             return view('users.index',compact('users'));
+        }
+        public function destroy(User $user){
+            $this->authorize('destroy',$user);
+            $user->delete();
+            session()->flash('success','成功刪除');
+            return back();
         }
 }
